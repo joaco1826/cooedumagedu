29c024a6c4327d2ac2197e1ff125651f316d1104
Joaquin Olaciregui <juaco.1826@gmail.com>
2017-12-11 11:11:23 -0500

    Fix Banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -180,7 +180,6 @@
             nextArrow: $("#der"),
             autoplay: true,
             autoplaySpeed: 5000,
-            fade: true,
             speed: 500,
             slidesToShow: 1,
             slidesToScroll: 1,
